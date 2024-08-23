aa639bad810c7419dbc9b802afb635a5ae9fc78c
JannelJ <janneljones26@gmail.com>
2024-08-23 11:37:40 +0100

    add image class

---
+++ dom-jannel/src/main/java/org/example/Image.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue
+    long id;
+
+    String fileName;
+    String fileData;
+    String associatedRecipe;
+
+}
