361044e75a31c729e06f2e7a2ed5038bf1866e6c
JannelJ <janneljones26@gmail.com>
2024-08-23 11:18:12 +0100

    feat: create sessionfactory and schemamanager

--- dom-jannel/src/main/java/org/example/Main.java
+++ dom-jannel/src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.relational.SchemaManager;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        SessionFactory sessionFactory = Database.getSessionFactory();
+        SchemaManager schemaManager = sessionFactory.getSchemaManager();
+
     }
 }
