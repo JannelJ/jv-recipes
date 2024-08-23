0e790cbf32929714782bc0c86bf864b85b9eddb1
JannelJ <janneljones26@gmail.com>
2024-08-23 11:32:53 +0100

    add category class

--- dom-jannel/src/main/java/org/example/Category.java
+++ dom-jannel/src/main/java/org/example/Category.java
@@ -1,17 +1,17 @@
 package org.example;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jdk.jfr.Name;
-
 
 @Entity
-public class Tag {
-    @Id
-    @GeneratedValue
-    long id;
+public class Category {
+        @Id
+        @GeneratedValue
+        long id;
 
-    String name;
+        String name;
 
 }
+
